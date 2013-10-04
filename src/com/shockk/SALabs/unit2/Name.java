5edeac7a05347db1a7398e7e002288dd714290cb
shockk <shokku.ra@gmail.com>
2013-10-04 14:50:42 +0100

    Wrote Name class and NameTest for unit2.

---
+++ src/com/shockk/SALabs/unit2/Name.java
@@ -0,0 +1,34 @@
+package com.shockk.SALabs.unit2;
+
+public class Name implements Comparable<Name>
+{
+	protected String forename;
+	protected String surname;
+	
+	public Name()
+	{
+		this.forename = Input.getString("Forename: ");
+		this.surname = Input.getString("Surname: ");
+	}
+	
+	@Override
+	public int compareTo(Name obj)
+	{
+		int result = this.forename.compareToIgnoreCase(obj.forename);
+		return (result == 0) ? this.surname.compareToIgnoreCase(obj.surname) : result;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(!(obj instanceof Name)) return false;
+		boolean result = this.forename.equalsIgnoreCase(((Name)obj).forename);
+		return result ? this.surname.equalsIgnoreCase(((Name)obj).surname) : result;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return this.forename + " " + this.surname;
+	}
+}
---
+++ src/com/shockk/SALabs/unit2/NameTest.java
@@ -0,0 +1,26 @@
+package com.shockk.SALabs.unit2;
+
+public class NameTest
+{
+	public static void main(String[] args)
+	{
+		while(true)
+		{
+			Name name1 = new Name();
+			Name name2 = new Name();
+			
+			System.out.println(name1);
+			System.out.println(name2);
+			System.out.println(name1.equals(name2));
+			
+			int result = name1.compareTo(name2);
+
+			if(result == 0)     System.out.println("equals");
+			else if(result > 0) System.out.println("greater than");
+			if(result < 0)      System.out.println("less than");
+			
+			String cont = Input.getString("Continue? (y/n): ");
+			if(cont.equalsIgnoreCase("n")) break;
+		}
+	}
+}
